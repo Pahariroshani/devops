7a5462ea9d4b20cf00700da4cc6b7a8c69f0554e
Pahariroshani <pahariroshani2000@gmail.com>
2022-09-25 10:19:59 -0400

    updated shell_script has been pushed

--- script.sh
+++ script.sh
@@ -2,3 +2,4 @@
 touch file1
 echo "hello world">>file1
 echo "Committing this file from local repository to remote repository">>file1
+echo " I am updating this file again after pushing in the local repository">>file1
